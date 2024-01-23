876731d3a3c893d51e170fe79bd59985c79f5d6c
lundqvisthampus <lundqvisthampus@outlook.com>
2024-01-23 15:37:23 +0100

    Added dbcontext and dependency injection

---
+++ MoviesApp/Contexts/MovieDatabaseContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using MoviesApp.Entities;
+namespace MoviesApp.Contexts;
+
+
+/// <summary>
+/// DbContext is responisble for interacting with the database.
+/// Following code is used for creating the tables in the database
+/// </summary>
+public class MovieDatabaseContext(DbContextOptions<MovieDatabaseContext> options) : DbContext(options)
+{
+    public virtual DbSet<ProductionCompanyEntity> ProductionCompanies { get; set; }
+    public virtual DbSet<GenreEntity> Genres { get; set; }
+    public virtual DbSet<DirectorEntity> Directors { get; set; }
+    public virtual DbSet<MovieProviderEntity> MovieProviders { get; set; }
+    public virtual DbSet<MovieEntity> Movies { get; set; }
+
+
+    /// <summary>
+    /// Makes sure that the chosen properties of each entity is unique in the database.
+    /// </summary>
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ProductionCompanyEntity>()
+            .HasIndex(x => x.CompanyName)
+            .IsUnique();
+
+        modelBuilder.Entity<GenreEntity>()
+            .HasIndex(x => x.GenreName)
+            .IsUnique();
+
+        modelBuilder.Entity<MovieProviderEntity>()
+            .HasIndex(x => x.ProviderName)
+            .IsUnique();
+    }
+}
--- MoviesApp/Program.cs
+++ MoviesApp/Program.cs
@@ -1,2 +1,12 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MoviesApp.Contexts;
+
+
+// Configuring dependency injection for the datacontext with connection string.
+var builder = Host.CreateDefaultBuilder().ConfigureServices(services =>
+{
+    services.AddDbContext<MovieDatabaseContext>(x => x.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Coding\EC-code\CSharp\EFCAssignment\MoviesApp\Data\MovieDatabase.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=True"));
+
+}).Build();

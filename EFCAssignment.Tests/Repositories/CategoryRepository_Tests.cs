8e7aef3be3f1b8afc35ad0d0779f83876756224f
lundqvisthampus <lundqvisthampus@outlook.com>
2024-02-05 18:45:08 +0100

    Added base repo and tests for base repo

---
+++ EFCAssignment.Tests/Repositories/CategoryRepository_Tests.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesApp.Contexts;
+using MoviesApp.Entities;
+using MoviesApp.Repositories;
+
+namespace EFCAssignment.Tests.Repositories;
+
+public class CategoryRepository_Tests
+{
+    private readonly ProductCatalogContext _context =
+    new(new DbContextOptionsBuilder<ProductCatalogContext>()
+    .UseInMemoryDatabase($"{Guid.NewGuid()}")
+    .Options);
+
+    [Fact]
+    public async Task CreateAsync_ShouldCreateAndAddEntityToDb_ReturnEntity()
+    {
+        // Arrange
+        var repo = new CategoryRepository(_context);
+        var entity = new Category { CategoryName = "Test" };
+
+        // Act
+        var result = await repo.CreateAsync(entity);
+
+        // Assert
+        Assert.NotNull(result);
+    }
+
+    [Fact]
+    public async Task GetOneAsync_ShouldGetOneEntityFromDb_ReturnEntity()
+    {
+        // Arrange
+        var repo = new CategoryRepository(_context);
+        var entity = new Category { CategoryName = "Test" };
+        await repo.CreateAsync(entity);
+
+        // Act
+        var result = await repo.GetOneAsync(x => x.CategoryName == entity.CategoryName);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsType<Category>(result);
+    }
+
+    [Fact]
+    public async Task GetAllAsync_ShouldGetAllEntitiesFromDb_ReturnList()
+    {
+        // Arrange
+        var repo = new CategoryRepository(_context);
+        var entity = new Category { CategoryName = "Test" };
+        await repo.CreateAsync(entity);
+
+        // Act
+        var result = await repo.GetAllAsync();
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsAssignableFrom<IEnumerable<Category>>(result);
+    }
+
+    [Fact]
+    public async Task UpdateAsync_ShouldUpdateEntityInDb_ReturnTrue()
+    {
+        // Arrange
+        var repo = new CategoryRepository(_context);
+        var entity = new Category { CategoryName = "Test" };
+        await repo.CreateAsync(entity);
+        entity.CategoryName = "Test2";
+
+        // Act
+        var result = await repo.UpdateAsync(x => x.Id == entity.Id, entity);
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public async Task DeleteAsync_ShouldDeleteEntityFromDb_ReturnTrue()
+    {
+        // Arrange
+        var repo = new CategoryRepository(_context);
+        var entity = new Category { CategoryName = "Test" };
+        await repo.CreateAsync(entity);
+
+        // Act
+        var result = await repo.DeleteAsync(entity);
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public async Task DeleteAsync_ShouldNotDeleteEntityFromDb_ReturnFalse()
+    {
+        // Arrange
+        var repo = new CategoryRepository(_context);
+        var entity = new Category { CategoryName = "Test" };
+
+        // Act
+        var result = await repo.DeleteAsync(entity);
+
+        // Assert
+        Assert.False(result);
+    }
+}
---
+++ MoviesApp/Repositories/CategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using MoviesApp.Contexts;
+using MoviesApp.Entities;
+
+namespace MoviesApp.Repositories;
+
+public class CategoryRepository : BaseRepository<Category>
+{
+    private readonly ProductCatalogContext _context;
+
+    public CategoryRepository(ProductCatalogContext context) : base(context)
+    {
+        _context = context;
+    }
+}
